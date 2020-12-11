93256173d4f4e23750243a9669a4a768a3a1c8cb
Beto Cortés <roberto.cortes13233@gmail.com>
2020-12-11 12:16:31 -0600

    Add /build

---
+++ build/precache-manifest.cae4613431dc0f3726948b91dd6fa607.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9d65953500bb452322d",
+    "url": "/platzi-badges/static/css/main.a3b6f5d9.chunk.css"
+  },
+  {
+    "revision": "c9d65953500bb452322d",
+    "url": "/platzi-badges/static/js/main.c9d65953.chunk.js"
+  },
+  {
+    "revision": "0f7117b146aebd482afe",
+    "url": "/platzi-badges/static/js/runtime~main.0f7117b1.js"
+  },
+  {
+    "revision": "1c8a786e7e3d84cf1a3c",
+    "url": "/platzi-badges/static/css/2.ed0eaddf.chunk.css"
+  },
+  {
+    "revision": "1c8a786e7e3d84cf1a3c",
+    "url": "/platzi-badges/static/js/2.1c8a786e.chunk.js"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/platzi-badges/static/media/Group 69.ed7c58ea.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/platzi-badges/static/media/estrellas.0abcdfcd.svg"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/platzi-badges/static/media/logo conf.a1880fbf.svg"
+  },
+  {
+    "revision": "b7872d684a27c591c436d0a67b1414ed",
+    "url": "/platzi-badges/static/media/beto.b7872d68.jpg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/platzi-badges/static/media/Group 47.6841730f.svg"
+  },
+  {
+    "revision": "50e7d89aab208d7983eb535ba4ba5240",
+    "url": "/platzi-badges/index.html"
+  }
+];
