97ce14fc806b93d2a3be515a97e8c4d61996944d
Beto Cortés <roberto.cortes13233@gmail.com>
2020-12-11 12:32:14 -0600

    Updates

--- precache-manifest.4eee2cf83dbbf20a16b3f07dc7eccfe3.js
+++ precache-manifest.4eee2cf83dbbf20a16b3f07dc7eccfe3.js
@@ -1,35 +1,35 @@
 self.__precacheManifest = [
   {
-    "revision": "c9d65953500bb452322d",
-    "url": "/platzi-badges/static/css/main.a3b6f5d9.chunk.css"
+    "revision": "24fdaa95efbeae607cfd",
+    "url": "/platzi-badges/static/css/main.80138ed1.chunk.css"
   },
   {
-    "revision": "c9d65953500bb452322d",
-    "url": "/platzi-badges/static/js/main.c9d65953.chunk.js"
+    "revision": "24fdaa95efbeae607cfd",
+    "url": "/platzi-badges/static/js/main.24fdaa95.chunk.js"
   },
   {
     "revision": "0f7117b146aebd482afe",
     "url": "/platzi-badges/static/js/runtime~main.0f7117b1.js"
   },
   {
-    "revision": "1c8a786e7e3d84cf1a3c",
+    "revision": "1e2147514d55957d8dc6",
     "url": "/platzi-badges/static/css/2.ed0eaddf.chunk.css"
   },
   {
-    "revision": "1c8a786e7e3d84cf1a3c",
-    "url": "/platzi-badges/static/js/2.1c8a786e.chunk.js"
+    "revision": "1e2147514d55957d8dc6",
+    "url": "/platzi-badges/static/js/2.1e214751.chunk.js"
   },
   {
     "revision": "ed7c58eafe17eff3b2557c0369b20264",
     "url": "/platzi-badges/static/media/Group 69.ed7c58ea.svg"
   },
   {
-    "revision": "0abcdfcd1e323649a0338a204fe8b853",
-    "url": "/platzi-badges/static/media/estrellas.0abcdfcd.svg"
-  },
-  {
     "revision": "a1880fbfb5634577b88090c9f82b3de7",
     "url": "/platzi-badges/static/media/logo conf.a1880fbf.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/platzi-badges/static/media/estrellas.0abcdfcd.svg"
   },
   {
     "revision": "b7872d684a27c591c436d0a67b1414ed",
@@ -40,7 +40,11 @@ self.__precacheManifest = [
     "url": "/platzi-badges/static/media/Group 47.6841730f.svg"
   },
   {
-    "revision": "50e7d89aab208d7983eb535ba4ba5240",
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/platzi-badges/static/media/astronautas.71addf45.svg"
+  },
+  {
+    "revision": "e7c10e4667d0cfea7b25a251429fcff3",
     "url": "/platzi-badges/index.html"
   }
 ];
